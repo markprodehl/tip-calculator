a8a7c2eff227484691423e9242b269e004761571
markprodehl <markprodehl@gmail.com>
2023-01-26 16:08:50 -0800

    initial commit with tip claculations that need improving

---
+++ src/TipCalculator.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+  
+const TipCalculator = () => {
+  const [totalTips, setTotalTips] = useState(0);
+  const [persons, setPersons] = useState([{ name: '', hours: 0 }]);
+  
+  function calculateShares() {
+    let totalHours = 0;
+    persons.forEach(person => {
+      totalHours += person.hours
+    });
+    
+    console.log("TOTAL HOURS", totalHours)
+    return persons.map(person => {
+      const share = (totalTips / totalHours) * person.hours;
+      return { ... person, share }
+    })
+  }
+
+  const shares = calculateShares()
+  
+  return (
+    <div>
+      <label> Total Tips:</label>
+      <input
+        type="number"
+        value={totalTips}
+        onChange={e => setTotalTips(e.target.value)}
+      />
+      <form onSubmit={e => e.preventDefault()}>
+        {persons.map((person, index) => (
+          <div key={index}>
+            <label>Name:</label>
+            <input
+              type="text"
+              value={person.name}
+              onChange={e => {
+                const newPersons = [...persons];
+                newPersons[index].name = e.target.value;
+                setPersons(newPersons);
+              }}
+            />
+            <label>Hours Worked:</label>
+            <input
+              type="number"
+              value={person.hours}
+              onChange={e => {
+                const newPersons = [...persons];
+                newPersons[index].hours = e.target.value;
+                setPersons(newPersons);
+              }}
+            />
+          </div>
+        ))}
+        <button 
+          type="button" 
+          onClick={() => setPersons([...persons, {name: "", hours: 0 }])}>
+          Add Person
+        </button>
+      </form>
+      <h3>Shares</h3>
+      <ul>
+        {shares.map((person, index) => (
+          <li key={index}> 
+            {person.name}: {person.share == 0 ? "" : person.share}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default TipCalculator;
