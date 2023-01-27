1880b9e4ca135757c3063b82a0d75ecd921d3a68
markprodehl <markprodehl@gmail.com>
2023-01-26 16:15:15 -0800

    Converted TipCalculator.js into the component folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TipCalculator from './TipCalculator';
+import TipCalculator from './components/TipCalculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
