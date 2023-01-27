4599edb5f9abfba8e2dee8ec1abdafcf2040bd65
markprodehl <markprodehl@gmail.com>
2023-01-26 17:08:00 -0800

    Updated the page layout to list the employees rather than display them side-by-side

--- src/components/TipCalculator.js
+++ src/components/TipCalculator.js
@@ -22,6 +22,7 @@ const TipCalculator = () => {
   
   return (
     <div>
+      <h2 className="header"> TIP Calculator</h2>
       <form className="form" onSubmit={e => e.preventDefault()}>
         <label className="tip-calculator">Total Tips:</label>
         <input
@@ -30,47 +31,49 @@ const TipCalculator = () => {
           value={totalTips}
           onChange={e => setTotalTips(e.target.value)}
         />
-          {persons.map((person, index) => (
-            <div key={index}>
-              <label>Name:</label>
-              <input
-                className="input"
-                type="text"
-                value={person.name}
-                onChange={e => {
-                  const newPersons = [...persons];
-                  newPersons[index].name = e.target.value;
-                  setPersons(newPersons);
-                }}
-              />
-              <label>Hours Worked:</label>
-              <input
-                className="input"
-                type="number"
-                value={person.hours}
-                onChange={e => {
-                  const newPersons = [...persons];
-                  newPersons[index].hours = e.target.value;
-                  setPersons(newPersons);
-                }}
-              />
-            </div>
-          ))}
+          <ul className="shares">
+            {persons.map((person, index) => (
+              <li key={index}>
+                <label>Name:</label>
+                <input
+                  className="input"
+                  type="text"
+                  value={person.name}
+                  onChange={e => {
+                    const newPersons = [...persons];
+                    newPersons[index].name = e.target.value;
+                    setPersons(newPersons);
+                  }}
+                />
+                <label>Hours Worked:</label>
+                <input
+                  className="input"
+                  type="number"
+                  value={person.hours}
+                  onChange={e => {
+                    const newPersons = [...persons];
+                    newPersons[index].hours = e.target.value;
+                    setPersons(newPersons);
+                  }}
+                />
+              </li>
+            ))}
+          </ul>
           <button 
             className="button"
             type="button" 
             onClick={() => setPersons([...persons, {name: "", hours: 0 }])}>
             Add Person
           </button>
+          <h3>Shares</h3>
+          <ul className="shares">
+            {shares.map((person, index) => (
+              <li key={index}> 
+                {person.name}: {person.share == 0 ? "" : person.share}
+              </li>
+            ))}
+          </ul>
       </form>
-      <h3>Shares</h3>
-      <ul className="shares">
-        {shares.map((person, index) => (
-          <li key={index}> 
-            {person.name}: {person.share == 0 ? "" : person.share}
-          </li>
-        ))}
-      </ul>
     </div>
   )
 }
