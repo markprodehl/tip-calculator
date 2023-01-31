2a0ce8cea11fa83491bd6e30c4c4669215f5a0fc
markprodehl <markprodehl@gmail.com>
2023-01-30 18:27:48 -0800

    Adjusted item order

--- src/components/TipCalculator.js
+++ src/components/TipCalculator.js
@@ -27,23 +27,28 @@ const TipCalculator = () => {
   };
 
   return (
-    <div>
-      <form>
+    
+    <div className="container">
+      <h2>Jason's Tronk Calculator</h2>
+      <form className="input-form">
         <input 
           type="number" 
           value={totalTips || ""} 
           onChange={handleTotalTipsChange} 
-          placeholder="Total Tips" />
-        <br />
-        <br />
+          placeholder="Total Tips" 
+          className="input-field"
+        />
+      <br />
       </form>
-      <form onSubmit={handleEmployeeSubmit}>
+      <br/>
+      <form onSubmit={handleEmployeeSubmit} className="input-form">
         <input
           type="text"
           name="employeeName"
           placeholder="Employee name"
           value={employeeName}
           onChange={(e) => setEmployeeName(e.target.value)}
+          className="input-field"
         />
         <input
           type="number"
@@ -51,28 +56,36 @@ const TipCalculator = () => {
           placeholder="Hours worked"
           value={hoursWorked}
           onChange={(e) => setHoursWorked(e.target.value)}
+          className="input-field"
         />
-        <button type="submit">Add Employee</button>
+        <button type="submit" className="add-btn">Add Employee</button>
       </form>
       <br />
-      {employees.map((employee) => (
-        <div key={employee.employeeName}>
-          {employee.employeeName}: {employee.hoursWorked} hours
-          <button onClick={() => handleDelete(employee)}>Delete</button>
-        </div>
-      ))}
+      <div className="total-hours">
+        Total Hours: {totalHours}
+      </div>
       <br />
-      Total hours: {totalHours}
-      <br />
-      <br />
-      Total tips per hour: {totalTips / totalHours}
-      <br />
-      <br />
-      {employees.map((employee) => (
-        <div key={employee.employeeName}>
-          {employee.employeeName}: {(totalTips / totalHours) * employee.hoursWorked}
-        </div>
-      ))}
+      <div className="total-tips-per-hour">
+        Average Tips Per Hour: {totalTips / totalHours}
+      </div>
+      <br/>
+      <div className="employee-list">
+        {employees.map((employee) => (
+          <div key={employee.employeeName} className="employee-item">
+            {employee.employeeName}: {employee.hoursWorked} hours - {(totalTips / totalHours) * employee.hoursWorked}
+            <button onClick={() => handleDelete(employee) } className="delete-btn">Delete</button>
+          </div>
+        ))}
+      </div>
+      {/* <br />
+      <br /> */}
+      {/* <div className="employee-tips">
+        {employees.map((employee) => (
+          <div key={employee.employeeName}>
+            {employee.employeeName}: {(totalTips / totalHours) * employee.hoursWorked}
+          </div>
+        ))}
+      </div> */}
     </div>
   );
 };
