fa1856a193db45888805e23e739bf7718aac9178
markprodehl <markprodehl@gmail.com>
2023-01-30 17:05:26 -0800

    Removed the App import from index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import TipCalculator from './components/TipCalculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <TipCalculator />
   </React.StrictMode>
 );
